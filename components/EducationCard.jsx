29ed42bb89fd5eeed81c18f239185686db0193bd
alfitof <alfito.fbriansyah@gmail.com>
2022-12-20 23:26:07 +0700

    Adding Education Page

---
+++ components/EducationCard.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Card, CardBody, Badge } from 'reactstrap';
+
+import { Fade } from 'react-reveal';
+
+const EducationCard = ({ education }) => {
+  return (
+    <Fade left duration={2000}>
+      <Card className="card-lift--hover shadow mt-4">
+        <CardBody>
+          <div className="d-flex px-3">
+            <div className="pl-4">
+              <h4 className="text-white">{education.schoolName}</h4>
+              <h6 className="text-white">{education.subHeader}</h6>
+              <Badge color="success" className="mr-1">
+                {education.duration}
+              </Badge>
+              {education.grade && (
+                <Badge color="primary" className="mr-1">
+                  {education.grade}
+                </Badge>
+              )}
+
+              <ul className="mt-3 text-white">
+                {education.descBullets
+                  ? education.descBullets.map((desc) => {
+                      return <li key={desc}>{desc}</li>;
+                    })
+                  : null}
+              </ul>
+            </div>
+          </div>
+        </CardBody>
+      </Card>
+    </Fade>
+  );
+};
+
+export default EducationCard;
