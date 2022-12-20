2b7667a4c16801b7c93c82bcabb4933647bcc40d
alfitof <alfito.fbriansyah@gmail.com>
2022-12-20 23:20:52 +0700

    Adding Skill Page

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
+import '../styles/styles.css';
 import '../styles/globals.css';
 import '../styles/vendor/font-awesome/css/font-awesome.min.css';
 import '../styles/vendor/nucleo/css/nucleo.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
 import dynamic from 'next/dynamic';
+const Navigation = dynamic(() => import('../components/Navigation'));
 const Greetings = dynamic(() => import('../containers/Greetings'));
+import SEO from '../components/SEO';
+import Skills from '../containers/Skills';
 
 export default function Home() {
-  return <Greetings />;
+  return (
+    <div>
+      <SEO />
+      {/* <Navigation /> */}
+      <Greetings />
+      <Skills />
+    </div>
+  );
 }
