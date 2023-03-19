e32f20e886c0d9886d01ee224bf1d69904f325a7
alfitof <alfito.fbriansyah@gmail.com>
2023-03-19 16:11:56 +0700

    Change position

--- portfolio.js
+++ portfolio.js
@@ -1,72 +1,76 @@
 export const greetings = {
-  name: 'Alfito Febriansyah',
+  name: "Alfito Febriansyah",
   title: "Hello guys, i'm Alfito!",
-  description: 'I am passionate as a Full Stack web developer having web application experience with PHP, Codeigniter, React.js, Next.js and CSS Framework with Tailwind and bootstrap.',
-  resumeLink: 'https://cv.aksel.co.id/r/7zgmwy',
+  description:
+    "I am passionate as a Software Engineer having web application experience with PHP, Codeigniter, React.js, Next.js and CSS Framework with Tailwind and bootstrap.",
+  resumeLink: "https://cv.aksel.co.id/r/7zgmwy",
 };
 
 export const openSource = {
-  githubUserName: 'alfitof',
+  githubUserName: "alfitof",
 };
 
 export const contact = {};
 
 export const socialLinks = {
-  linkedin: 'https://www.linkedin.com/in/alfito-febriansyah/',
-  github: 'https://github.com/alfitof',
-  instagram: 'https://www.instagram.com/needsomesauce',
-  facebook: 'https://www.facebook.com/alfito.febriansyah',
-  twitter: 'https://twitter.com/atlantisbanjir',
+  linkedin: "https://www.linkedin.com/in/alfito-febriansyah/",
+  github: "https://github.com/alfitof",
+  instagram: "https://www.instagram.com/needsomesauce",
+  facebook: "https://www.facebook.com/alfito.febriansyah",
+  twitter: "https://twitter.com/atlantisbanjir",
 };
 
 export const skillsSection = {
-  title: 'Skills',
+  title: "Skills",
   data: [
     {
-      title: 'Full Stack Development',
-      lottieAnimationFile: '/lottie/skills/fullstack.json',
-      skills: ['Building Responsive E-Commerce in Codeigniter', 'Building responsive portfolio websites using Next.js'],
+      title: "Software Engineer",
+      lottieAnimationFile: "/lottie/skills/fullstack.json",
+      skills: [
+        "Building Responsive E-Commerce in Codeigniter",
+        "Building responsive portfolio websites using Next.js",
+      ],
       softwareSkills: [
         {
-          skillName: 'HTML-5',
-          fontAwesomeClassname: 'vscode-icons:file-type-html',
+          skillName: "HTML-5",
+          fontAwesomeClassname: "vscode-icons:file-type-html",
         },
         {
-          skillName: 'CSS-3',
-          fontAwesomeClassname: 'vscode-icons:file-type-css',
+          skillName: "CSS-3",
+          fontAwesomeClassname: "vscode-icons:file-type-css",
         },
         {
-          skillName: 'JavaScript',
-          fontAwesomeClassname: 'logos:javascript',
+          skillName: "JavaScript",
+          fontAwesomeClassname: "logos:javascript",
         },
         {
-          skillName: 'Reactjs',
-          fontAwesomeClassname: 'vscode-icons:file-type-reactjs',
+          skillName: "Reactjs",
+          fontAwesomeClassname: "vscode-icons:file-type-reactjs",
         },
         {
-          skillName: 'Nextjs',
-          fontAwesomeClassname: 'vscode-icons:file-type-light-next',
+          skillName: "Nextjs",
+          fontAwesomeClassname: "vscode-icons:file-type-light-next",
         },
         {
-          skillName: 'PHP',
-          fontAwesomeClassname: 'vscode-icons:file-type-php',
+          skillName: "PHP",
+          fontAwesomeClassname: "vscode-icons:file-type-php",
         },
         {
-          skillName: 'Codeigniter',
-          fontAwesomeClassname: 'logos:codeigniter-icon',
+          skillName: "Codeigniter",
+          fontAwesomeClassname: "logos:codeigniter-icon",
         },
 
         {
-          skillName: 'MySQL',
-          fontAwesomeClassname: 'logos:mysql',
+          skillName: "MySQL",
+          fontAwesomeClassname: "logos:mysql",
         },
         {
-          skillName: 'Tailwind',
-          fontAwesomeClassname: 'vscode-icons:file-type-tailwind',
+          skillName: "Tailwind",
+          fontAwesomeClassname: "vscode-icons:file-type-tailwind",
         },
         {
-          skillName: 'Bootstrap',
-          fontAwesomeClassname: 'logos:bootstrap',
+          skillName: "Bootstrap",
+          fontAwesomeClassname: "logos:bootstrap",
         },
       ],
     },
@@ -75,43 +79,48 @@ export const skillsSection = {
 
 export const SkillBars = [
   {
-    Stack: 'Frontend',
-    progressPercentage: '70',
+    Stack: "Frontend",
+    progressPercentage: "70",
   },
   {
-    Stack: 'Backend',
-    progressPercentage: '55',
+    Stack: "Backend",
+    progressPercentage: "55",
   },
   {
-    Stack: 'Design',
-    progressPercentage: '75',
+    Stack: "Design",
+    progressPercentage: "75",
   },
 ];
 
 export const educationInfo = [
   {
-    schoolName: 'SMAN 1 Ciamis',
-    subHeader: 'Science',
-    duration: '2017 - 2020',
-    grade: '85.71',
-    descBullets: ['Join the OPTIK organization (Information Technology Student Organization)'],
+    schoolName: "SMAN 1 Ciamis",
+    subHeader: "Science",
+    duration: "2017 - 2020",
+    grade: "85.71",
+    descBullets: [
+      "Join the OPTIK organization (Information Technology Student Organization)",
+    ],
   },
   {
-    schoolName: 'Telkom University',
-    subHeader: 'Bachelor of Information Technology',
-    duration: '2020 - 2024',
-    grade: '3.65',
-    descBullets: ['Participated in web creation for SMEs in the IT profession course.', 'Did an internship at Advanced Software Engineer Laboratory'],
+    schoolName: "Telkom University",
+    subHeader: "Bachelor of Information Technology",
+    duration: "2020 - 2024",
+    grade: "3.66",
+    descBullets: [
+      "Participated in web creation for SMEs in the IT profession course.",
+      "Did an internship at Advanced Software Engineer Laboratory",
+    ],
   },
 ];
 
 export const experience = [
   {
-    role: 'Intern Frontend Developer',
-    company: 'Advanced Software Engineer',
-    companylogo: '/img/icons/common/logo192.png',
-    date: 'Mei 2022 – Des 2022',
-    desc: 'I intern as frontend web developer to slicing website Advanced Software Engineer design using ReactJS, Tailwind, and some other libraries.',
+    role: "Intern Frontend Developer",
+    company: "Advanced Software Engineer",
+    companylogo: "/img/icons/common/logo192.png",
+    date: "Mei 2022 – Des 2022",
+    desc: "I intern as frontend web developer to slicing website Advanced Software Engineer design using ReactJS, Tailwind, and some other libraries.",
   },
 ];
 
@@ -120,10 +129,18 @@ export const showContactUs = true;
 
 // See object prototype on SEO.jsx page
 export const seoData = {
-  title: 'Alfito Febriansyah',
-  description: 'A Junior Fullstack Developer',
-  author: 'Alfito Febriansyah',
-  image: 'https://avatars.githubusercontent.com/u/91746558?v=4',
-  url: 'https://portfolio-alfitof.vercel.app',
-  keywords: ['needsomesauce', 'Alfito Febriansyah', '@needsomesauce', 'alfitof', 'Portfolio', 'Alfito Portfolio ', 'Alfito Febriansyah Portfolio'],
+  title: "Alfito Febriansyah",
+  description: "A Junior Fullstack Developer",
+  author: "Alfito Febriansyah",
+  image: "https://avatars.githubusercontent.com/u/91746558?v=4",
+  url: "https://portfolio-alfitof.vercel.app",
+  keywords: [
+    "needsomesauce",
+    "Alfito Febriansyah",
+    "@needsomesauce",
+    "alfitof",
+    "Portfolio",
+    "Alfito Portfolio ",
+    "Alfito Febriansyah Portfolio",
+  ],
 };
